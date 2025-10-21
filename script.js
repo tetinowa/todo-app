8cdb590279557c20be55cac66e7bfaeab9f9157c
Inowa <tmrgaunt@gmail.com>
2025-10-21 08:50:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ addButton.addEventListener("click", () => {
     list.innerHTML = value;
   }
 });
+
+
+const list = document.querySelector(".list");
+const addButton = document.querySelector(".task-add-button");
+const input = document.getElementById("textInput");
+
+addButton.addEventListener("click", () => {
+  const value = input.value.trim();
+  if (value === "") return;
+
+  const li = document.createElement("li");
+
+  const textSpan = document.createElement("span");
+  textSpan.textContent = value;
+  textSpan.addEventListener("click", () => {
+    textSpan.classList.toggle("done");
+  });
+
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "❌";
+  delBtn.classList.add("delete-btn");
+  delBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
+
+  li.appendChild(textSpan);
+  li.appendChild(delBtn);
+
+  list.appendChild(li);
+  input.value = "";
+});
